5d814af1cc95e5bbd2d37d1b4f09b46327199777
DiegoSamayoaGomez <samayoasg2014@gmail.com>
2025-08-16 13:36:57 -0600

    Feat: create a 2D array which will serve as a gameboard

--- battleship.js
+++ battleship.js
@@ -21,6 +21,25 @@ function Ship(length) {
   };
 }
 
+function Gameboard() {
+  const rows = 10;
+  const columns = 10;
+  const board = [];
+
+  // Create 2D array that will represent the state of the game board
+  for (let i = 0; i < rows; i++) {
+    board[i] = [];
+    for (let j = 0; j < columns; j++) {
+      board[i][j] = "-";
+    }
+  }
+
+  // Retrieve the current gameboard
+  const getBoard = () => board;
+
+  return { getBoard };
+}
+
 // This functions should be passed to a test
 //const ship = Ship;
 /*
@@ -31,4 +50,7 @@ console.log(ship.isSunk()); // false
 ship.hit();
 console.log(ship.isSunk()); // true
  */
-module.exports = { Ship };
+//module.exports = { Ship };
+
+const game = Gameboard();
+console.table(game.getBoard());
